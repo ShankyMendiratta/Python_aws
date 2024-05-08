3f183bf0aaa6716a5e398ea9d8a9633a8ffbf719
Shanky Mendiratta <95498131+ShankyMendiratta@users.noreply.github.com>
2024-05-08 22:07:11 +0530

    aws_kinesis_describe.py

---
+++ aws_kinesis_describe.py
@@ -0,0 +1,2 @@
+import botocore  # Declaring the botocore package or module. AWS Botocore provides low level interface to AWS services and is foundation for boto3 library as well AWS CLI.
+import boto3 
